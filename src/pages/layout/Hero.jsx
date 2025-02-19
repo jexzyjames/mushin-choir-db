7360e2eb145d189170e3e6eb5ad7847c5bba44dd
jexzyjames <jexzyjames@gmail.com>
2025-02-19 11:50:25 +0100

    worked on logout modal

--- src/pages/layout/Hero.jsx
+++ src/pages/layout/Hero.jsx
@@ -23,7 +23,7 @@ const Hero = () => {
   const age = useSelector((state) => state.auth.user.age);
   const group = useSelector((state) => state.auth.user.group);
   const part = useSelector((state) => state.auth.user.part);
-
+  const [modal,setModal] = useState(false);
   const dispatch = useDispatch();
   const [open, setOpen] = useState(false);
   const [head, sethead] = useState("DASHBOARD");
@@ -77,13 +77,41 @@ const Hero = () => {
                   <h1 className='flex gap-1' >Hi! <p className="text-yellow-600 font-bold " >{user.name}</p></h1>
                   <button
                     onClick={() => {
-                      logout();
-                      setOpen(false);
+                      // setOpen(false);
+                      setModal(true)
                     }}
                     className=" flex justify center m-auto items-center gap-1 text-center w-full bg-gray-300 font-extrabold  rounded-md shadow-md p-1 "
                   >
                    <FaArrowAltCircleRight/> Sign out
                   </button>
+                  {/* Modal for Logout */}
+                  
+                      {modal ? 
+                  <div className='absolute top-0 m-auto z-50 flex justify-center items-center  right-0 left-0 bottom-0 bg-slate-700   opacity-100  w-full  text-black' >
+
+                       <div className='bg-white  absolute top-0 right-0 left-0  bottom-0  text-black flex  flex-col  text-center p-3  rounded-md shadow-md '>
+                       <h1 className='md:text-xl mb-2 uppercase ' >Are you sure you want to log out</h1>
+                       <h2 className='text-yellow-800 font-extrabold md:text-xl ' >{user.name}</h2>
+                       <div className='w-full flex gap-4 justify-center mt-3 mb-3 items-center '>
+                           <button onClick={()=> {
+                              setModal(false)
+                               dispatch(logoutUser())
+                               navigate('/login')
+                           }} className='text-white w-full rounded-md shadow-md p-2 text-md  bg-sky-500' >Yes</button>
+                           <button onClick={()=> {
+                               setModal(false)
+                               setOpen(false)
+                               navigate('/dash')
+                              
+                           }} className='text-white w-full rounded-md shadow-md p-2 text-md  bg-red-500' >NO</button>
+                       </div>
+                   </div> 
+                   </div>
+                   : 
+                   ''
+                   
+                  }
+                  
                 </div>
               )}
             </div>
