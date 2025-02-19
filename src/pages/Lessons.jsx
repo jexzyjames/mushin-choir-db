bee6706b1d63665d00a0251ffe30bc0e9b1b5396
jexzyjames <jexzyjames@gmail.com>
2025-02-19 11:36:54 +0100

    revamped the lessons component

--- src/pages/Lessons.jsx
+++ src/pages/Lessons.jsx
@@ -32,14 +32,14 @@ const Lessons = ({ title }) => {
             </div>
 
             {assignments?.lessons && assignments.lessons.length > 0 ? (
-              assignments.lessons.map((assignment) => (
+              assignments.lessons.map((lessons) => (
                 <div className="bg-neutral-100 mb-2 p-2 md:p-4 rounded-xl shadow-lg ">
                   <h1 className="text-2xl md:w-full  text-amber-800 mt-3 font-extrabold text-left">
-                    {assignment.title}
+                    {lessons.title}
                   </h1>
 
                   <p className="mb-3 text-[13px]  md:w-full md:text-md font-bold  ">
-                    {assignment.content}
+                    {lessons.content}
                   </p>
                 </div>
               ))
